2b1096de18deacd167ff42300554f3d0bc5878e3
shunsong <yushunsong@126.com>
2026-03-19 15:44:22 +0800

    feat:初始化前端框架

---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  );
+}
